ff73230ea0c64030847022548848fcbc85985642
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-01-19 21:04:25 +0100

    peticiones centralizadas ya hechas en servicios

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,35 +14,36 @@ export class SpotifyService {
 
   constructor(private http: HttpClient) { }
 
-  getQuery(query:string){
-    
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQB1_MNh0lnFEfVajjn8aPlZiW1FAhXwcc8pvaqOmTPzVcJ-A0KfmgV9YBrx9FRdjtB_rN__OvxgTisNP3A'
+    });
+
+    return this.http.get(url, { headers })
   }
 
   getNewReleases() {
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDtXWBJg3uizGOrwB9YxO8w8v1JEpm4qSynOQ9tQa8KRbjdotKQn9zhbOmfEKgbplvo9ePoMgQc9HZCNuI'
-    });
-
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-              .pipe(map((data:any)=>{
-                return data.albums.items;
-              }));
+    // const headers = new HttpHeaders({
+    //   'Authorization': 'Bearer BQB1_MNh0lnFEfVajjn8aPlZiW1FAhXwcc8pvaqOmTPzVcJ-A0KfmgV9YBrx9FRdjtB_rN__OvxgTisNP3A'
+    // });
+    return this.getQuery('browse/new-releases')
+      .pipe(map(data => data['albums'].items));
 
   }
 
 
 
-    getArtista(termino: string){
+  getArtista(termino: string) {
 
-      const headers = new HttpHeaders({
-        'Authorization': 'Bearer BQDtXWBJg3uizGOrwB9YxO8w8v1JEpm4qSynOQ9tQa8KRbjdotKQn9zhbOmfEKgbplvo9ePoMgQc9HZCNuI'
-      });
-  
-      return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers })
-                  .pipe(map((data:any)=>{
-                    return data.artists.items;
-                  }));
+    // const headers = new HttpHeaders({
+    //   'Authorization': 'Bearer BQB1_MNh0lnFEfVajjn8aPlZiW1FAhXwcc8pvaqOmTPzVcJ-A0KfmgV9YBrx9FRdjtB_rN__OvxgTisNP3A'
+    // });
 
-    }
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
+      .pipe(map(data => data['artists'].items));
+
+  }
 }
